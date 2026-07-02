51c71742f29b4fd50b678c85deba1f46cff77f10
alexdp-dp <alex@digitalpartners.ro>
2026-07-02 12:35:13 +0300

    Update User-Agent and log server response
    Updated the User-Agent string for requests and added a print statement to display the server header.

--- checker.py
+++ checker.py
@@ -3,13 +3,13 @@ import requests
 
 
 USER_AGENT = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/137.0.0.0 Safari/537.36"
-    )
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1"
 }
-
 
 def check_site(site):
 
@@ -45,6 +45,7 @@ def check_site(site):
             headers=USER_AGENT,
             allow_redirects=True
         )
+        print(response.headers.get("Server"))
         print(site["name"])
         print(response.status_code)
         print(response.url)
